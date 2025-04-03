20b5d3498c3460018e00c84f52087ed094be69d1
Kavya982 <medakavya0982@gmail.com>
2025-04-03 21:47:13 +0530

    Add files via upload

---
+++ stringreverse.py
@@ -0,0 +1,16 @@
+st = input("Enter string = ")
+
+# print(st[::-1])
+
+res=""
+# for i in st:
+#     res=i+res 
+# print(res)
+
+# for i in range(0,len(st)):
+#     res=st[i]+res
+# print(res)
+
+# lst=list(st)
+# lst.reverse()
+# print("".join(lst))
